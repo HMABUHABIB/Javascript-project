7a86d7efed12852bb33470bd369aa54c04e8e624
HMABUHABIB <46394953+HMABUHABIB@users.noreply.github.com>
2020-12-18 14:10:43 +0100

    Add files via upload

---
+++ 12-birthday-counter/app.js
@@ -0,0 +1,66 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020, 11, 22, 00, 00, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const day = futureDate.getDay();
+const weekday = weekdays[day];
+const date = futureDate.getDate();
+let month = futureDate.getMonth();
+month = months[month];
+giveway.textContent = `${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+// future time in ms 
+const futureTime = futureDate.getTime();
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+  //values in ms
+  const onnDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  let days = t / onnDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % onnDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 100);
+  console.log(days);
+  console.log(hours);
+  console.log(minutes);
+
+  items[0].innerHTML = days;
+  items[1].innerHTML = hours;
+  items[2].innerHTML = minutes;
+  items[3].innerHTML = seconds;
+
+}
+getRemainingTime();
+
